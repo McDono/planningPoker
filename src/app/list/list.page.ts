be3d892c2528d7644dbcefc57ba8544eb21911a8
Donovan <mcdonovan@outlook.fr>
2019-09-27 19:23:30 -0500

    checkbox is finally working properly. I'm proud of me right now

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -12,56 +12,22 @@ export class ListPage implements OnInit {
 	// itemsInterface = this.cardService.optionsInterface;
 	// icons = this.cardService.icons;
 	items = this.cardService.items;
+	isCheckboxChecked = false;
 
   constructor(private cardService: CardService,
 							private router: Router) { }
 
   ngOnInit() {
-		// var checkbox = document.getElementsByTagName("ion-checkbox")[0];
-		// var path = "";
-		// checkbox.addEventListener("ionChange", (e) => {
-		// 	if (e.target.checked === true)
-				// path = "/home";
-				// this.router.navigate(["/home"]);
-			// else
-				// path = "/list";
-				// this.router.navigate(["/list"]);
-			// var list = document.getElementById("list");
-			//list.removeAttribute("routerLink");
-			// list.setAttribute("routerLink", path);
-			// console.log(list.getAttribute("routerLink"));
-
-		// });
   }
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
 
 	onSelect(item) {
-		//console.log(item);
 		this.cardService.setSelection(item);
-		var checkbox = document.getElementsByTagName("ion-checkbox")[0];
-		if (checkbox.getAttribute("checked") === true)
+		if (!this.isCheckboxChecked)
 			this.router.navigate(["/home"]);
-			//----------------------------------I'M HERE--------------------
 	}
 
-	// selectRouter() {
-	// 	return "/home";
-	// }
-
-	checkboxClicked() {
-		// var checkbox = document.getElementsByTagName("ion-checkbox")[0];
-		// var list = document.getElementById("list");
-		// console.log(list);
-		// var path = "";
-		// if (checkbox.getAttribute("checked") === true)
-		// 	path = "/home";
-		// else
-		// 	path = "/list";
-		// console.log(path);
-		// list.setAttribute("routerLink", path);
-		// console.log(list.getAttribute("routerLink"));
+	checkboxClicked(e) {
+		this.isCheckboxChecked = e.currentTarget.checked;
+		console.log(this.isCheckboxChecked);
 	}
 }
